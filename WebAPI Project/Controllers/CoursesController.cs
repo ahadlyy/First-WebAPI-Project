3c31118531a2661260f59c723d3b99f00491ecd9
AhmedAdly <157370741+ahadlyy@users.noreply.github.com>
2024-04-22 00:25:49 +0200

    Add project files.

---
+++ WebAPI Project/Controllers/CoursesController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI_Project.Models;
+
+namespace WebAPI_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoursesController : ControllerBase
+    {
+        ITIContext db;
+        public CoursesController(ITIContext db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllCourses()
+        {
+            var courses = db.Courses.ToList();
+            if (courses == null || courses.Count() == 0)
+            {
+                return NotFound();
+            }
+            return Ok(courses);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetCourseById(int id)
+        {
+            var course = db.Courses.Find(id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+            return Ok(course);
+        }
+
+        [HttpGet("{name:alpha}")]
+        public IActionResult GetCourseByName(string crsname)
+        {
+            var course = db.Courses.FirstOrDefault(c=>c.Crs_Name == crsname);
+            if (course == null)
+            {
+                return NotFound();
+            }
+            return Ok(course);
+        }
+
+        [HttpPost]
+        public IActionResult AddCourse(Course course)
+        {
+            if (course == null)
+            {
+                return BadRequest();
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            db.Courses.Add(course);
+            db.SaveChanges();
+            return CreatedAtAction("GetCourseById", new { id = course.Crs_Id }, course);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCourse(int id)
+        {
+            var course = db.Courses.SingleOrDefault(x => x.Crs_Id == id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+            db.Courses.Remove(course);
+            db.SaveChanges();
+            return Ok(course);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateCourse(int id, Course c)
+        {
+            if (c == null)
+            {
+                return NotFound();
+            }
+            if (id != c.Crs_Id)
+            {
+                return BadRequest();
+            }
+            var existingCourse = db.Courses.FirstOrDefault(course => course.Crs_Id == id);
+
+            if (existingCourse == null)
+            {
+                return NotFound();
+            }
+            db.Entry(c).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            db.SaveChanges();
+            return Ok(c);
+        }
+    }
+}
---
+++ WebAPI Project/Controllers/StudentController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI_Project.Models;
+
+namespace WebAPI_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        ITIContext db;
+        public StudentController(ITIContext db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet]
+
+        public List<Student> GetallStudents() 
+        {
+            return db.Students.ToList();
+        }
+
+        [HttpGet("{id:int}")]
+
+        public IActionResult Getstudentbyid(int id) 
+        {
+            Student st = db.Students.Find(id);
+            if(st == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(st);
+            }
+        }
+
+        [HttpGet("/api/std/{fname}")]
+        //[HttpGet("{fname:alpha}")]
+
+        public IActionResult Getstudentfname(string fname)
+        {
+            Student st = db.Students.FirstOrDefault(n => n.St_Fname == fname);
+            if(st == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(st);
+            }
+        }
+
+        [HttpPost]
+
+        public IActionResult addstudent(Student st) 
+        {
+            if(st == null)
+            {
+                return BadRequest("please enter the required data");
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("please enter correct data");
+            }
+            db.Students.Add(st);
+            db.SaveChanges();
+            return CreatedAtAction("Getstudentbyid",new {id = st.St_Id},st); // returns url for the added student
+        }
+
+        [HttpPut("{id}")]
+
+        public IActionResult editstudent (Student st,[FromRoute] int id) //atribute to spcify from where get the data
+        {
+            if(st == null)
+            {
+                return BadRequest();
+            }
+            if(st.St_Id != id) 
+            {
+                return BadRequest();
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            db.Entry(st).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            db.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete]
+
+        public IActionResult deletestudent(int id)
+        {
+            Student st = db.Students.Find(id);
+            if(st == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                db.Students.Remove(st);
+                db.SaveChanges();
+                return Ok(st);
+            }
+        }
+    }
+}
